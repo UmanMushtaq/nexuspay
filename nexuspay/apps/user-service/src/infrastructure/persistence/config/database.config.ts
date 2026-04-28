d3d0102a5118af258cfce50119e572684a224e55
Uman Mushtaq <umanmushtaq72@gmail.com>
2026-04-28 02:56:49 +0200

    feat(user-service): configure TypeORM in UserModule
    - Updated database config to use ORM entities
    - Connected TypeOrmModule to UserModule
    - Disabled synchronize (preparing for migrations)

--- nexuspay/apps/user-service/src/infrastructure/persistence/config/database.config.ts
+++ nexuspay/apps/user-service/src/infrastructure/persistence/config/database.config.ts
@@ -1,6 +1,8 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { User } from "../../../domain/entities/user.entity";
 import { Kyc } from "../../../domain/entities/kyc.entity";
+import { UserOrmEntity } from "../entities/user.orm-entity";
+import { KycOrmEntity } from "../entities/kyc.orm-entity";
 
 
 
@@ -10,8 +12,8 @@ host: 'localhost',
 port: 5432,
 username: 'postgres',
 password: 'password',
-entities: [User, Kyc],
-synchronize: true, // Disable in production 
+entities: [UserOrmEntity, KycOrmEntity],
+synchronize: false, // Disable in production 
 logging:true,
 autoLoadEntities: true
 }
