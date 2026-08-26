177df5833f729ac826b3445adb6cc96d16633dba
Uman Mushtaq <umanmushtaq72@gmail.com>
2026-08-26 02:41:16 +0200

    fix(notification-service): fix database password in config

--- apps/notification-service/src/infrastructure/persistence/config/database.config.ts
+++ apps/notification-service/src/infrastructure/persistence/config/database.config.ts
@@ -7,7 +7,7 @@ export const databaseConfig: TypeOrmModuleOptions = {
   host: process.env.DB_HOST || 'localhost',
   port: 5435,
   username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
   database: 'notification_db',
   entities: [NotificationOrmEntity],
   synchronize: true,
