cee4402092f21bee4dfe224acc627d111e289a70
Uman Mushtaq <umanmushtaq72@gmail.com>
2026-08-28 00:38:36 +0200

    feat(api-gateway): forward auth headers and add missing user routes

--- apps/api-gateway/src/infrastructure/controllers/user.controller.ts
+++ apps/api-gateway/src/infrastructure/controllers/user.controller.ts
@@ -1,33 +1,65 @@
-import { Body, Controller, HttpCode, HttpStatus, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Headers, Param, Post } from "@nestjs/common";
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from "rxjs";
+import { serviceUrls } from "../config/service-urls.config";
 
 @Controller('users')
-export class UserController{
-    constructor(private readonly httpService:HttpService){}
+export class UserController {
+    constructor(private readonly httpService: HttpService) {}
 
     @Post('register')
     @HttpCode(HttpStatus.CREATED)
-    async register(@Body() body:any){
-        const response:any = await firstValueFrom(this.httpService.post('http://localhost:3001/users/register', body))
-       
+    async register(@Body() body: any) {
+        const response: any = await firstValueFrom(
+            this.httpService.post(`${serviceUrls.user}/users/register`, body)
+        );
         return response.data;
     }
 
     @Post('login')
-        @HttpCode(HttpStatus.OK)
-  async login(@Body() body: any) {
-    const response:any = await firstValueFrom(
-      this.httpService.post('http://localhost:3001/users/login', body)
-    );
-    return response.data;
-  }
+    @HttpCode(HttpStatus.OK)
+    async login(@Body() body: any) {
+        const response: any = await firstValueFrom(
+            this.httpService.post(`${serviceUrls.user}/users/login`, body)
+        );
+        return response.data;
+    }
 
-  @Post(':id/kyc')
-  async submitKyc(@Param('id') id: string, @Body() body: any) {
-    const response:any = await firstValueFrom(
-      this.httpService.post(`http://localhost:3001/users/${id}/kyc`, body)
-    );
-    return response.data;
-  }
+    @Get('me')
+    async getProfile(@Headers('authorization') authHeader: string) {
+        const response: any = await firstValueFrom(
+            this.httpService.get(`${serviceUrls.user}/users/me`, {
+                headers: { Authorization: authHeader },
+            })
+        );
+        return response.data;
+    }
+
+    @Post(':id/kyc')
+    async submitKyc(
+        @Param('id') id: string,
+        @Body() body: any,
+        @Headers('authorization') authHeader: string,
+    ) {
+        const response: any = await firstValueFrom(
+            this.httpService.post(`${serviceUrls.user}/users/${id}/kyc`, body, {
+                headers: { Authorization: authHeader },
+            })
+        );
+        return response.data;
+    }
+
+    @Post(':id/kyc/review')
+    async reviewKyc(
+        @Param('id') id: string,
+        @Body() body: any,
+        @Headers('authorization') authHeader: string,
+    ) {
+        const response: any = await firstValueFrom(
+            this.httpService.post(`${serviceUrls.user}/users/${id}/kyc/review`, body, {
+                headers: { Authorization: authHeader },
+            })
+        );
+        return response.data;
+    }
 }
